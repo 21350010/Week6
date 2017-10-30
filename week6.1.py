503107a4c700c491a3304a460690fc8e93fb8974
21350010 <32065584+21350010@users.noreply.github.com>
2017-10-30 13:32:55 +0000

    week6.1
    scoreA and soreB must be defined

---
+++ week6.1.py
@@ -0,0 +1,10 @@
+
+#Finding the errors in the following statement
+##if scoreA = scoreB :  
+##        print("Tie")
+
+scoreA=int(input("scoreA :"))
+scoreB=int(input("scoreB :"))
+
+if scoreA == scoreB :
+    print("Tie")
