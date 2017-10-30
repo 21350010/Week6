a54046237d01b232a8a0a634ef1022de55c6ed38
21350010 <32065584+21350010@users.noreply.github.com>
2017-10-30 13:43:41 +0000

    week6.2
    if . . . // supply statement

---
+++ week6.2.py
@@ -0,0 +1,9 @@
+##Supplying a condition to if statement to test if the user entered a “Y”:
+##	userInput = input("Enter Y to quit.")
+##	if . . . // supply statement
+##		print("Goodbye")    // if the user entered “Y”
+
+userInput = input("Enter Y to quit:")
+
+if userInput=="y":
+    print("Goodbye")    
