68cc43c4aa8f7d831088e19ebee311f3fd1b7fe5
21350010 <32065584+21350010@users.noreply.github.com>
2018-01-12 18:53:52 +0000

    Update week6.3.4.py

---
+++ week6.3.4.py
@@ -0,0 +1,14 @@
+
+grade= int(input("Enter Marks: "))
+
+letterGrade = "F"
+if grade >= 90 :
+	letterGrade = "A"
+if grade >= 80 :
+	letterGrade = "B"
+if grade >= 70 :
+	letterGrade = "C"
+if grade >= 60 :
+	letterGrade = "D"
+
+print(letterGrade)
